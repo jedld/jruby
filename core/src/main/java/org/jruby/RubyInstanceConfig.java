d6974ce39314d2c9f006db55f0e86665bbbe7ef3
Joseph Dayo <jdayo@friendster.com>
2014-01-03 12:11:39 +0800

    dalvik compatibility patch

--- core/src/main/java/org/jruby/RubyInstanceConfig.java
+++ core/src/main/java/org/jruby/RubyInstanceConfig.java
@@ -1795,19 +1795,7 @@ public class RubyInstanceConfig {
     ////////////////////////////////////////////////////////////////////////////
     
     private static int initGlobalJavaVersion() {
-        String specVersion = Options.BYTECODE_VERSION.load();
-        
-        // stack map calculation is failing for some compilation scenarios, so
-        // forcing both 1.5 and 1.6 to use 1.5 bytecode for the moment.
-        if (specVersion.equals("1.5")) {// || specVersion.equals("1.6")) {
-           return Opcodes.V1_5;
-        } else if (specVersion.equals("1.6")) {
-            return Opcodes.V1_6;
-        } else if (specVersion.equals("1.7") || specVersion.equals("1.8")) {
-            return Opcodes.V1_7;
-        } else {
-            throw new RuntimeException("unsupported Java version: " + specVersion);
-        }
+        return Opcodes.V1_6;
     }
 
     @Deprecated
